a72a462f26aa4565b83349b5649b7b0be72bb82e
realisticshigzo <oluwasegunoluwabiyi12@gmail.com>
2026-08-29 04:05:06 +0100

    changes2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Africa-Education-Icon-Award/',
   plugins: [
     react({ compiler: true }),
     tailwindcss(),
